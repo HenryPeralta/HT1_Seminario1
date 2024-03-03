39e03c03d1e3965988543e4d0324a7353d2caf11
HenryPeralta <henrygabrielperalta@gmail.com>
2024-03-02 18:31:19 -0600

    Cambio de puerto
    Cambio de puerto

--- ServidorNode/index.js
+++ ServidorNode/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 4000);
 
 app.use(cors())
 
